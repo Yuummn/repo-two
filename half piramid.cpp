fe3ef615e096513db1bacfd5307559582a80b98e
Yuummn <89058285+Yuummn@users.noreply.github.com>
2022-06-25 00:08:18 +0700

    Add files via upload

---
+++ half piramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int rows;
+
+    cout<<"Masukkan nomer baris: ";
+    cin>>rows;
+
+    for(int i=1; i<=rows; ++i)
+    {
+        out<<"\n";
+        }
+    return 0;
+    }
